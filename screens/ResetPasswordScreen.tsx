1b5d02b4ad904f62af9a5dddc8d1dd578e5c809d
Yuming Liu <jercy_lew@yahoo.com>
2022-06-03 16:28:07 +0800

    Feature: Setting new password

--- screens/ResetPasswordScreen.tsx
+++ screens/ResetPasswordScreen.tsx
@@ -3,56 +3,87 @@ import {
   View,
   Image,
   StyleSheet,
-  StatusBar,
+  Text,
+  TextInput,
   ImageBackground,
+  TouchableOpacity,
+  Alert,
 } from "react-native";
 import { Button } from 'react-native-paper';
 
 import { HomeScreenProps } from "../types";
 
 const ResetPasswordScreen = ({ route, navigation }: HomeScreenProps) => {
+  const [newPassword, setNewPassword] = React.useState('');
+  const [confirmPassword, setConfirmPassword] = React.useState('');
+
   return (
     <View style={styles.container}>
       <ImageBackground
         source={require("../assets/images/background.png")}
-        resizeMode="cover"
+        resizeMode="repeat"
         style={styles.image}
       >
-        <StatusBar backgroundColor="#FF6347" barStyle="light-content" />
         <View style={styles.main}>
-          <View style={styles.logo}>
-            <Image source={require("../assets/images/logo.png")}></Image>
-          </View>
-          <View>
+          <View
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+              marginTop: 0,
+            }}
+          >
+            <Text style={{ color: "white", fontSize: 30, fontWeight: "bold" }}>
+            Nueva contraseña
+            </Text>
+            <Text style={{ color: "white", fontSize: 18,
+            paddingTop: 5,  width: 350, textAlign: 'center', fontWeight: 'bold'  }}>
+            Por favor, introduzca su nueva contraseña para continuar
+            </Text>
+            <View style={[styles.inputContainer, {marginTop: 50}]}>
+              <Text style={styles.inputPrefix}>{`    `}</Text>
+              <TextInput
+                secureTextEntry={true}
+                onChangeText={(text) => {
+                  setNewPassword(text);
+                }}
+                value={newPassword}
+                keyboardType={"default"}
+                autoComplete={"password"}
+                style={styles.input}
+                placeholder="Nueva Contraseña"
+                placeholderTextColor={"white"}
+              />
+            </View>
+            <View style={styles.inputContainer}>
+              <Text style={styles.inputPrefix}>{`    `}</Text>
+              <TextInput
+                secureTextEntry={true}
+                onChangeText={(text) => {
+                  setConfirmPassword(text);
+                }}
+                value={confirmPassword}
+                keyboardType={"default"}
+                autoComplete={"password"}
+                style={styles.input}
+                placeholder="Confirmar Contraseña"
+                placeholderTextColor={"white"}
+              />
+            </View>
             <Button
               mode="contained"
               onPress={() => {
-                console.log("Jumping to Sign In page");
-                navigation.navigate("SignInScreen");
+                console.log("Resetting password ...");
+                Alert.alert('Resetting Password', 'New password was set successfully!');
+                // navigation.navigate("SignInScreen");
               }}
               color={"white"}
-              contentStyle={{ height: 80, width: 280 }}
-              labelStyle={{ fontSize: 22 }}
-              style={styles.signInButton}
+              contentStyle={{ height: 70, width: 350 }}
+              labelStyle={{ fontSize: 22, color: '#C93E54' }}
+              style={[styles.signInButton, { marginTop: 30 }]}
               uppercase={false}
             >
-              Iniciar sesión
+              Siguiente
             </Button>
-            <View>
-              <Button
-                mode="contained"
-                onPress={() => {
-                  console.log("Jumpng to Create a new account page");
-                  navigation.navigate("SignUpScreen");
-                }}
-                contentStyle={{ height: 80, width: 280 }}
-                labelStyle={{ fontSize: 22 }}
-                style={styles.signUpButton}
-                uppercase={false}
-              >
-                Crear una cuenta
-              </Button>
-            </View>
           </View>
         </View>
       </ImageBackground>
@@ -68,7 +99,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#FF6347",
   },
   logo: {
-    paddingHorizontal: 30,
+    // paddingHorizontal: 30,
   },
   main: {
     flex: 1,
@@ -80,14 +111,29 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   signInButton: {
-    marginTop: 50,
+    marginTop: 20,
     borderRadius: 50,
   },
-  signUpButton: {
-    marginTop: 50,
+  input: {
+    width: 280,
+    fontSize: 20
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginHorizontal: 10,
     borderRadius: 50,
-    borderWidth: 2,
-    borderColor: "white",
+    height: 70,
+    margin: 6,
+    padding: 10,
+    fontSize: 20,
     backgroundColor: "rgba(52, 52, 52, 0.1)",
+    borderWidth: 2,
+    borderColor: 'white',
+  },
+  inputPrefix: {
+    paddingHorizontal: 5,
+    fontSize: 20,
+    color: '#9AAAAA',
   },
 });
